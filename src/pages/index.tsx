e4e4bbf84519d1ade05f8979b9e1d1900237f956
Petar Strbad <petar.strbad@factoryxhrvatska.onmicrosoft.com>
2024-07-09 09:13:16 +0200

    remove uncessary page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,15 @@
 import { THEME_TYPE } from "@/axiosfolder";
 import BullionmarkShop from "@/components/partials/shop/Bullionmark";
-import QmintShop from "@/components/partials/shop/Qmint";
 import React from "react";
 
 // Create a wrapper component to handle conditional rendering
 const ShopWrapper = (props: any) => {
-  if (THEME_TYPE == "1") {
-    return <BullionmarkShop {...props} />;
-  } else {
-    return <QmintShop {...props} />;
-  }
+  return <BullionmarkShop />;
 };
 
 // Export getServerData from the component that will be used
 export const getServerData = async (context: any) => {
-  if (THEME_TYPE == "1") {
-    return BullionmarkShop.getServerData(context);
-  } else {
-    return QmintShop.getServerData(context);
-  }
+  return BullionmarkShop.getServerData(context);
 };
 
 export default ShopWrapper;
